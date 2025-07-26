bafb115b576dda1acd2c40e2c85c632faf3c9f6f
sungjoonyoung <164128698+sungjoonyoung@users.noreply.github.com>
2025-07-26 20:06:33 +0900

    Create SCC.cpp

---
+++ SCC.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<algorithm>
+using namespace std;
+vector<int> s;
+int p[100001];
+bool fix[100001];
+int ind=1;
+int DFS(int x){
+    p[x]=ind;
+    int pnum=ind;
+    s.push_back(x);
+    ind++;
+    for(int i=0;i<v[x].size();i++){
+        if(!p[v[x][i]])pnum=min(pnum,DFS(v[x][i]));
+        else if(!fix[v[x][i]])pnum=min(pnum,p[v[x][i]]);
+    }
+    if(p[x]==pnum){
+        while(s.back()!=x){
+            fix[s.back()]=1;
+            p[s.back()]=x;
+            s.pop_back();
+        }
+        fix[s.back()]=1;
+        p[s.back()]=x;
+        s.pop_back();
+    }
+    return pnum;
+}
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+}
